1e0d9a320891757af9a2a35411f34975c58dbe58
bnebur <ruben.briceno.f@gmail.com>
2022-06-18 23:51:58 -0500

    Uso de funciones en js

--- test2.js
+++ test2.js
@@ -4,4 +4,18 @@ const laCaja = null;
 const mediaDelCloset = 2.10 ;
 
 let porcentajeDeCarga = 85;
-let juegoPresente = runescape;
+let juegoPresente = 'runescape';
+
+
+console.log(porcentajeDeCarga);
+
+
+function primerTest(valor){
+	let palabra = "Hola Mundo"
+	console.log(valor,": " + palabra );
+
+}
+primerTest(1);
+primerTest(2);
+primerTest(3);
+primerTest(4);
