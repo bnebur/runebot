7e06108967b51349fc5d64032578c344a9db22ad
mskdmage <49457732+mskdmage@users.noreply.github.com>
2022-06-20 20:44:22 -0500

    Bug hierro revisado

--- index.js
+++ index.js
@@ -5,30 +5,33 @@ const objetos = require('./utils/objetos.js');
 
 const bot = new Bot(robot);
 
-while (1 === 1) {
+// while (1 === 1) {
 
-	let inventarioVacio = bot.si(bot.situacion.inventarioNoFull);
+// 	let inventarioVacio = bot.si(bot.situacion.inventarioNoFull);
 	
-	if (inventarioVacio) {
-		bot.viajar(ruta.deBancoAMina);
-		let hayEspacio = bot.si(bot.situacion.inventarioNoFull);
-		while (hayEspacio) {
-			bot.minar(objetos.hierro, 5);
-			hayEspacio = bot.si(bot.situacion.inventarioNoFull);
-		}
-		bot.minar(objetos.hierro, 4);
-	} else {
-		bot.viajar(ruta.deMinaABanco);
-		let banquero = bot.buscar(objetos.banquero);
-		if (banquero) {
-			bot.abrirMenuBanco(objetos.banquero);
-			bot.vaciarInventario();
-			bot.dormir(4);
-		}
-	}
+// 	if (inventarioVacio) {
+// 		bot.viajar(ruta.deBancoAMina);
+// 		let hayEspacio = bot.si(bot.situacion.inventarioNoFull);
+// 		while (hayEspacio) {
+// 			bot.minar(objetos.hierro, 5);
+// 			hayEspacio = bot.si(bot.situacion.inventarioNoFull);
+// 		}
+// 		bot.minar(objetos.hierro, 4);
+// 	} else {
+// 		bot.viajar(ruta.deMinaABanco);
+// 		let banquero = bot.buscar(objetos.banquero);
+// 		if (banquero) {
+// 			bot.abrirMenuBanco(objetos.banquero);
+// 			bot.vaciarInventario();
+// 			bot.dormir(4);
+// 		}
+// 	}
 
-}
+// }
 
+const x = ['211a2e', '181321', '0e0918', '0f0a19', '160e21', '191125', '1b1625', '251d32', '221b2f', '211a2e', '181321', '0e0918', '211a2b', '302641', '1e1729']
+y = new Set(x);
+console.log(y);
 
 
 
--- utils/objetos.js
+++ utils/objetos.js
@@ -1,19 +1,19 @@
 const banquero = {
-	color: ['211a2e', '181321', '0e0918', '0f0a19', '160e21', '191125', '1b1625', '251d32', '221b2f', '211a2e', '181321', '0e0918', '211a2b', '302641', '1e1729']
+	color: ['211a2e', '181321', '0e0918', '0f0a19', '160e21', '191125', '1b1625', '251d32', '221b2f', '211a2b', '302641', '1e1729']
 }
 	
 
 const hierro = {
-	color : ['452319', '2f190f', '3d1f16', '22110c']
+	color : ['452319', '3d1f16', '22110c', '391e14']
 }
 
 
 const cobre = {
 	color : ['744b2a', '4f321b', '714929' ]
 }
+
 module.exports = {
 	banquero : banquero,
 	hierro : hierro,
 	cobre : cobre
-
 }
