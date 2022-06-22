7919c52f9497f545c7b162e2748dc6a8fb68a04e
bnebur <ruben.briceno.f@gmail.com>
2022-06-22 00:11:39 -0500

    Ruta realizada para viajar del banco de AlKharid hasta la mina mas proxima

--- test2.js
+++ test2.js
@@ -1,21 +1,20 @@
-console.log("hola mundo");
+const robot = require('robotjs');
+const {Bot} = require('./utils/bot.js');
+const {ruta} = require('./utils/rutas.js');
+const objetos = require('./utils/objetos.js');
 
-const laCaja = null; 
-const mediaDelCloset = 2.10 ;
+const bot = new Bot(robot);
 
-let porcentajeDeCarga = 85;
-let juegoPresente = 'runescape';
-
-
-console.log(porcentajeDeCarga);
+bot.rectificar();
+bot.viajar(ruta.deBancoaMinaAlKharid);
 
 
-function primerTest(valor){
-	let palabra = "Hola Mundo"
-	console.log(valor,": " + palabra );
 
-}
-primerTest(1);
-primerTest(2);
-primerTest(3);
-primerTest(4);
+
+
+
+
+
+
+
+
