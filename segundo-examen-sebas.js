d489b2a7f2770df3ac5b1311b77ec1a903f238ec
freiseb <106829060+freiseb@users.noreply.github.com>
2022-06-29 23:42:13 -0500

    segundo examen listo para hacer calificado De:sebas

--- segundo-examen-sebas.js
+++ segundo-examen-sebas.js
@@ -8,21 +8,50 @@
 //		e) Tiene una funcion llamada potenciar, esta recibe un argumento. Dentro de la funcion eleva el atributo x al valor y lo asigna al atributo x. Al final llama mostrar.
 // IMPLEMENTAR AQUI
 
+class Calculadora{
+	constructor(x=0){
+		this.numero = x;
+	}
+	mostrar(){
+		console.log(this.numero);
+	}
+	sumar(cualquierNum){
+		this.numero += cualquierNum;
+		this.mostrar();
+	}
+	multiplicar(){
+		this.numero *= cualquierNum;
+		this.mostrar();
+	}
+	potenciar(y){
+		this.numero **= y;
+		this.mostrar();
+	}
+}
 
 // 2. Crear dos instancias de Calculadora, una llamada tiempo con el valor 2 y otra con el nombre potencia y valor 10.
 // IMPLEMENTAR AQUI
 
+let tiempo = new Calculadora(2);
+let potencia = new Calculadora(10);
 
 // 3. Realizar las siguientes operaciones:
 //		a) Usando los metodos que definimos llevar al cuadrado a ambas instancias.
 //		b) Sumar 23 a ambas instancias.
 // IMPLEMENTAR AQUI
 
-
+tiempo.potenciar(2);
+potencia.potenciar(2);
 // 4. Crear un array llamado numeros, que contiene 4 instancias de la clase Calculadora, con los valores 1, 2, 3, 4.
 // IMPLEMENTAR AQUI
+
+let numeros = [new Calculadora(1), new Calculadora(2), new Calculadora(3), new Calculadora(4)];
 
 
 // 5. Usando un bucle for, incrementar el valor de cada instancia del array numeros en 1.
 // IMPLEMENTAR AQUI
 
+
+for(let calc of numeros){
+	calc.sumar(1);
+}
